4dc7f7370f591198e9f26e28fa77ce015be8dde3
Andrei Kazakov <ay.kazakov89@gmail.com>
2021-10-26 19:05:25 +0200

    Implement naive table listing

--- src/main.rs
+++ src/main.rs
@@ -24,33 +24,42 @@ fn main() -> Result<()> {
     let command = &args[2];
     match command.as_str() {
         ".dbinfo" => {
-            // Parse page header from database
-            let page_header = PageHeader::parse(&database[100..108])?;
-
-            // Obtain all cell pointers
-            let cell_pointers = database[108..]
-                .chunks_exact(2)
-                .take(page_header.number_of_cells.into())
-                .map(|bytes| u16::from_be_bytes(bytes.try_into().unwrap()))
-                .collect::<Vec<_>>();
-
-            // Obtain all records from column 5
-            #[allow(unused_variables)]
-            let schemas = cell_pointers
-                .into_iter()
-                .map(|cell_pointer| {
-                    let stream = &database[cell_pointer as usize..];
-                    let (_, offset) = parse_varint(stream);
-                    let (_rowid, read_bytes) = parse_varint(&stream[offset..]);
-                    parse_record(&stream[offset + read_bytes..], 5)
-                        .map(|record| Schema::parse(record).expect("Invalid record"))
-                })
-                .collect::<Result<Vec<_>>>()?;
-
+            let schemas = get_schemas(&database)?;
             println!("number of tables: {}", schemas.len());
+        }
+        ".tables" => {
+            let schemas = get_schemas(&database)?;
+            println!(
+                "{}",
+                schemas
+                    .into_iter()
+                    .filter(|s| s.kind == "table" && s.table_name != "sqlite_sequence")
+                    .map(|s| s.table_name)
+                    .collect::<Vec<String>>()
+                    .join(" ")
+            );
         }
         _ => bail!("Missing or invalid command passed: {}", command),
     }
 
     Ok(())
+}
+
+fn get_schemas(database: &[u8]) -> Result<Vec<Schema>> {
+    // Parse page header from database
+    let page_header = PageHeader::parse(&database[100..108])?;
+
+    // Obtain all cell pointers
+    database[108..]
+        .chunks_exact(2)
+        .take(page_header.number_of_cells.into())
+        .map(|bytes| u16::from_be_bytes(bytes.try_into().unwrap()))
+        .map(|cell_pointer| {
+            let stream = &database[cell_pointer as usize..];
+            let (_, offset) = parse_varint(stream);
+            let (_rowid, read_bytes) = parse_varint(&stream[offset..]);
+            parse_record(&stream[offset + read_bytes..], 5)
+                .map(|record| Schema::parse(record).expect("Invalid record"))
+        })
+        .collect::<Result<Vec<_>>>()
 }
